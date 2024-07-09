f8d8d039648f0c5c27162a7135ac67e31a6e39d3
Ketan Chauhan <98975157+Ketan9548@users.noreply.github.com>
2024-07-09 05:24:39 +0000

    done

---
+++ basic_01/variable_js_2.js
@@ -0,0 +1,23 @@
+const user_Name = "Ketan"
+let user_Password = 432212
+var user_Email = "ketan@gmail.com"
+user_City = "jaspur"
+
+// console.log(user_Name)
+
+// new methode for print the variable
+// console.table([user_Name,user_Password,user_Email,user_City])
+
+// const is not change
+// user_Name = "sumit"
+// console.log(user_Name)
+
+// var is not good to use in js becouse of is we have to change the var variable in js then it's will be reflected in all the code snipped
+
+let b = 231
+let a = 432
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
