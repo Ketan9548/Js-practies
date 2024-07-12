75f5b0f32ef72258281edb508816b6ffcaba65ce
Ketan Chauhan <98975157+Ketan9548@users.noreply.github.com>
2024-07-12 11:08:36 +0000

    done

---
+++ basic_02/objects_3.js
@@ -0,0 +1,13 @@
+// Single letor
+
+// Objects Letor
+
+// Object.create(); this is make using constructor
+
+// object litrals
+const JsUser = {
+    name:"kapil",
+    number:"23122",
+    location:"kapil@gmail.com",
+    
+}
