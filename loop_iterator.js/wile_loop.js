6e7ab3b0e55dd0526a91c6219d833dd421dbaaee
Ketan Chauhan <98975157+Ketan9548@users.noreply.github.com>
2024-08-09 09:15:08 +0000

    done

--- loop_iterator.js/wile_loop.js
+++ loop_iterator.js/wile_loop.js
@@ -12,3 +12,28 @@
     // console.log(index)
     // index = index+4
 // } while (index < 20);
+
+
+const arr = [
+    {
+        name:"kapil",
+        password1:"321121",
+        username:"Kapil@321"
+    },
+    {
+        name:"sumit",
+        password2:"986534",
+        username:"sumit@321"
+    },
+    {
+        name:"prince",
+        password3:"98765",
+        username:"prince@321"
+    }
+]
+
+arr.forEach((item,index,value)=>{
+    if(index == 1){
+        console.log(value[index].username);
+    }
+})
