2d4cbb95df5609f0ed7d911ce552ea5f62a46f7b
Ketan Chauhan <98975157+Ketan9548@users.noreply.github.com>
2024-09-18 10:05:31 +0000

    done

--- Array_advance/First.js
+++ Array_advance/First.js
@@ -1,7 +1,144 @@
-function sum(a,b,d) {
-    return a+b+d;
-}
+// function sum(a,b,d) {
+//     return a+b+d;
+// }
 
-const value = [1,1,1];
+// const value = [1,1,1];
 
-console.log(sum(...value))
+// console.log(sum(...value))
+
+
+let arr = [
+    {
+      "id": 1,
+      "name": "Shania Prosacco",
+      "company": "Jayne",
+      "username": "Kaia_Stehr",
+      "email": "Delphine_Prosacco89@yahoo.com",
+      "address": "737 Altenwerth Drive",
+      "zip": "82282",
+      "state": "Montana",
+      "country": "Bangladesh",
+      "phone": "(958) 902-4027 x037",
+      "photo": "https://json-server.dev/ai-profiles/0.png"
+    },
+    {
+      "id": 2,
+      "name": "Ida Connelly",
+      "company": "Jabari",
+      "username": "May.Goldner",
+      "email": "Brielle.Schuppe@yahoo.com",
+      "address": "155 Dach Forest",
+      "zip": "94267",
+      "state": "Delaware",
+      "country": "United States of America",
+      "phone": "942.551.9186",
+      "photo": "https://json-server.dev/ai-profiles/1.png"
+    },
+    {
+      "id": 3,
+      "name": "Kayden Nolan",
+      "company": "Bennie",
+      "username": "Dustin.Ryan",
+      "email": "Haley35@hotmail.com",
+      "address": "4914 The Orchard",
+      "zip": "90046-4708",
+      "state": "New Hampshire",
+      "country": "South Sudan",
+      "phone": "500-714-1133 x6563",
+      "photo": "https://json-server.dev/ai-profiles/1.png"
+    },
+    {
+      "id": 4,
+      "name": "Yasmeen O&#x27;Reilly",
+      "company": "Autumn",
+      "username": "Lucas.Murray92",
+      "email": "Abigale.Stoltenberg55@gmail.com",
+      "address": "42782 Cleve Trail",
+      "zip": "78776",
+      "state": "Alaska",
+      "country": "New Zealand",
+      "phone": "613.984.0622",
+      "photo": "https://json-server.dev/ai-profiles/1.png"
+    },
+    {
+      "id": 5,
+      "name": "Shakira Terry",
+      "company": "Jermain",
+      "username": "Christop_Mante89",
+      "email": "Lemuel.Ankunding68@gmail.com",
+      "address": "46896 Lueilwitz Spring",
+      "zip": "58372",
+      "state": "Florida",
+      "country": "Luxembourg",
+      "phone": "1-239-955-0931",
+      "photo": "https://json-server.dev/ai-profiles/1.png"
+    },
+    {
+      "id": 6,
+      "name": "Madeline O&#x27;Connell-Yundt",
+      "company": "Jermaine",
+      "username": "Ellie_Beier0",
+      "email": "Salvador.Pfeffer81@hotmail.com",
+      "address": "2879 Field Close",
+      "zip": "00765-0244",
+      "state": "Montana",
+      "country": "Western Sahara",
+      "phone": "825.360.9824 x9200",
+      "photo": "https://json-server.dev/ai-profiles/1.png"
+    },
+    {
+      "id": 7,
+      "name": "Torrey Keebler",
+      "company": "Thurman",
+      "username": "Douglas.Reinger",
+      "email": "Kenna.Effertz@hotmail.com",
+      "address": "718 Bahringer Extension",
+      "zip": "93944-1049",
+      "state": "Vermont",
+      "country": "Norfolk Island",
+      "phone": "(810) 215-0306 x94609",
+      "photo": "https://json-server.dev/ai-profiles/1.png"
+    },
+    {
+      "id": 8,
+      "name": "Riley Pacocha",
+      "company": "Albertha",
+      "username": "Precious2",
+      "email": "Lew39@gmail.com",
+      "address": "455 Jamir Lights",
+      "zip": "39397",
+      "state": "Kansas",
+      "country": "Maldives",
+      "phone": "566-988-6409 x020",
+      "photo": "https://json-server.dev/ai-profiles/1.png"
+    },
+    {
+      "id": 9,
+      "name": "Roy Schoen",
+      "company": "Millie",
+      "username": "Tate.Simonis67",
+      "email": "Cory.Grady@hotmail.com",
+      "address": "979 Rohan Fall",
+      "zip": "52937",
+      "state": "West Virginia",
+      "country": "Iran",
+      "phone": "741.377.0791 x4863",
+      "photo": "https://json-server.dev/ai-profiles/1.png"
+    },
+    {
+      "id": 10,
+      "name": "Major Gutmann",
+      "company": "Lacey",
+      "username": "Raul_Hudson",
+      "email": "Larissa.Bayer34@yahoo.com",
+      "address": "2848 Myrtis Village",
+      "zip": "14840-6020",
+      "state": "Delaware",
+      "country": "Austria",
+      "phone": "1-958-982-2521",
+      "photo": "https://json-server.dev/ai-profiles/1.png"
+    }
+  ]
+
+
+  console.log(arr[0].company)
