135283110f2c3f84c18707e09a54edbd58ef316d
Ketan Chauhan <98975157+Ketan9548@users.noreply.github.com>
2024-11-11 05:55:13 +0000

    done

--- basic_01/variable_js_2.js
+++ basic_01/variable_js_2.js
@@ -1,7 +1,7 @@
-const user_Name = "Ketan"
-let user_Password = 432212
-var user_Email = "ketan@gmail.com"
-user_City = "jaspur"
+const user_Name = "Ketan";
+let user_Password = 432212;
+var user_Email = "ketan@gmail.com";
+user_City = "jaspur";
 
 // console.log(user_Name)
 
@@ -14,18 +14,30 @@ user_City = "jaspur"
 
 // var is not good to use in js becouse of is we have to change the var variable in js then it's will be reflected in all the code snipped
 
-let b = 231
-let a = 432
+let b = 231;
+let a = 432;
 
 // console.log(a+b)
 // console.log(a-b)
 // console.log(a*b)
 // console.log(a/b)
 
-
 // undefinde mening
-let score
-
+let score;
 
 // if we have not to give any value in variable then it's undefinde
-console.log(score)
+// console.log(score)
+
+// var scope
+// for (var i = 0; i < 5; i++) {
+//   setTimeout(() => {
+//     console.log("value of i is: ", i);
+//   }, 0);
+// }
+
+// let scope
+for (let i = 1; i <= 5; i++) {
+  setTimeout(() => {
+    console.log("value of i is: ", i);
+  }, 0);
+}
