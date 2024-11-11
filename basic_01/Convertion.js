24afa244fdba858a5536f18c9eb5ca307fbb5cb6
Ketan Chauhan <98975157+Ketan9548@users.noreply.github.com>
2024-11-11 06:34:33 +0000

    done

--- basic_01/Convertion.js
+++ basic_01/Convertion.js
@@ -6,5 +6,29 @@ let score = "33asabm"
 
 let val = Number(score);
 
-console.log(typeof(val))
-console.log(val)
+// console.log(typeof(val))
+// console.log(val)
+// console.log(Number(null))
+
+
+// boolean
+// "" -> false
+// "kapil" -> true
+// 1 -> true && 0 -> false 
+
+// ****************************** operation ***********************
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"3") // convertion 1+2 = 3 and 3+3 => 33 by string
+
+// prifix and postfix
+
+// let a = 100
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+
+// conparision operators
+console.log("2">1)
+console.log(null>1)
---
+++ basic_01/Datatype_summery.js
@@ -0,0 +1,20 @@
+// primative
+
+// 7 types = String, Number , Boolean , null , undefined , Symbol, BigInt
+
+// Refrence Type
+
+//  Array , Objects , Function
+
+//  js is Dynamic type language 
+// reasone : let score = 32 // not declayer the number or int
+
+// Cpp is the Static Type language 
+// resone : int a = 332; // declayer the int or other datatype in the declaration in the any datatype
+
+// Symbole 
+const id = Symbol("123")
+const anotherId = Symbol("123")
+//  Both are diffrence Symbole
+
+console.log(id === anotherId)
