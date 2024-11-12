53177ef064705e84cf5a042de7723152c07ff023
Ketan Chauhan <98975157+Ketan9548@users.noreply.github.com>
2024-11-12 06:55:18 +0000

    done

---
+++ basic_02/01_function.js
@@ -0,0 +1,26 @@
+function printvalue() {
+  console.log("kapil");
+  console.log("kapil");
+  console.log("kapil");
+  console.log("kapil");
+  console.log("kapil");
+  console.log("kapil");
+  console.log("kapil");
+}
+
+// printvalue()
+
+function addtonumber(number1, number2) {
+  console.log(number1 + number2);
+}
+
+// const value  = addtonumber(2,3)
+// console.log(value)
+
+function addtonumber(number1, number2) {
+  // console.log(number1+number2)
+  return number1 + number2;
+}
+
+const val = addtonumber(2, 3);
+console.log(val);
