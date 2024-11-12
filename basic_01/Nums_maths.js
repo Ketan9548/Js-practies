328f10d53928746481dd4275ad0524e94b1043bb
Ketan Chauhan <98975157+Ketan9548@users.noreply.github.com>
2024-11-12 05:02:08 +0000

    done

---
+++ basic_01/Nums_maths.js
@@ -0,0 +1,33 @@
+const score = 100;
+
+// console.log(score);
+// console.log(score.toString());
+
+const value = 100.2211212121212;
+// console.log(value.toFixed(2));
+
+
+const othernumber= 23.89112
+// console.log(othernumber.toPrecision(3))
+
+const number = 100000000
+// console.log(number.toLocaleString('en-IN'))
+
+
+// *********************** Maths ************************
+
+// console.log(Math)
+// console.log(Math.abs(-1))
+// console.log(Math.round(2.8))
+// console.log(Math.ceil(2.2))
+// console.log(Math.floor(2.8))
+// console.log(Math.min(3,2,5,1,9))
+
+
+// console.log((Math.random()*10)+1)
+
+const min = 10;
+const max = 20;
+
+const randomevalue = Math.floor(Math.random()*(max-min+1)+min)
+console.log(randomevalue)
--- basic_01/date_5.js
+++ basic_01/date_5.js
@@ -2,8 +2,22 @@
 
 const todaydate = new Date();
 
-console.log(todaydate)
-console.log(todaydate.toDateString())
-console.log(todaydate.toLocaleDateString())
-console.log(todaydate.toISOString())
-console.log(todaydate.toJSON())
+// console.log(todaydate)
+// console.log(todaydate.toDateString())
+// console.log(todaydate.toLocaleDateString())
+// console.log(todaydate.toLocaleString())
+// console.log(todaydate.toISOString())
+// console.log(todaydate.toJSON())
+
+
+let mytimestamp = Date.now();
+// console.log(mytimestamp);
+
+
+let newdate = new Date();
+// console.log(newdate.getHours())
+newdate.toLocaleString('default',{
+    weekday:'long'
+})
+
+// console.log(newdate)
