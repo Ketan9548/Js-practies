c096352c566c4f1bf48657350b54003f14273c8c
Ketan Chauhan <98975157+Ketan9548@users.noreply.github.com>
2024-12-17 05:20:55 +0000

    done

--- Advance_Topic/API_Fetch.js
+++ Advance_Topic/API_Fetch.js
@@ -9,15 +9,33 @@
 //     console.log("promis consumed")
 // })
 
-fetch('https://randomuser.me//api')
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-        // console.log("type of:", typeof (data));
-        const user = data;
-        console.log("the value is: ", user.info.page)
-        console.log("The second value is: ",user.results[0].gender)
-    }).catch((error) =>
-        console.log(error))
+
+// Promise
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let a = 100002;
+        let b = 100;
+        for (let i = 0; i < a; i++) {
+            let val = b*i;
+            console.log(val);
+        }
+        resolve();
+    },100)
+}).then(function(){
+    console.log("promis consumed")
+})
+
+
+
+// fetch('https://randomuser.me//api')
+//     .then((response) => {
+//         return response.json();
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         // console.log("type of:", typeof (data));
+//         const user = data;
+//         console.log("the value is: ", user.info.page)
+//         console.log("The second value is: ",user.results[0].gender)
+//     }).catch((error) =>
+//         console.log(error))
