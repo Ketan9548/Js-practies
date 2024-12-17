57faa61b9b96df072c0f735f5d506fe4ed030240
Ketan Chauhan <98975157+Ketan9548@users.noreply.github.com>
2024-12-17 05:53:24 +0000

    Done

--- Advance_Topic/API_Fetch.js
+++ Advance_Topic/API_Fetch.js
@@ -9,23 +9,42 @@
 //     console.log("promis consumed")
 // })
 
-
 // Promise
-new Promise(function(resolve,reject){
-    setTimeout(function(){
-        let a = 100002;
-        let b = 100;
-        for (let i = 0; i < a; i++) {
-            let val = b*i;
-            console.log(val);
-        }
-        resolve();
-    },100)
-}).then(function(){
-    console.log("promis consumed")
-})
+// new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let a = 100002;
+//         let b = 100;
+//         for (let i = 0; i < a; i++) {
+//             let val = b*i;
+//             console.log(val);
+//         }
+//         resolve();
+//     },100)
+// }).then(function(){
+//     console.log("promis consumed")
+// })
 
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = false;
+    if (!error) {
+      resolve("Successful Promise resolved");
+    } else {
+      reject("is Accured");
+    }
+  }, 1000);
+});
 
+async function consumePromise() {
+  try {
+    const val = await promiseFive;
+    console.log(val);
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
+consumePromise();
 
 // fetch('https://randomuser.me//api')
 //     .then((response) => {
